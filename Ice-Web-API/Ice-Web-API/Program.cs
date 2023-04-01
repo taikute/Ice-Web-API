b4e547fcfb70f2be630adb1739295fd997b4600b
Cyan <anhtai05102002@gmail.com>
2023-04-01 19:36:25 +0700

    Test API

--- Ice-Web-API/Ice-Web-API/Program.cs
+++ Ice-Web-API/Ice-Web-API/Program.cs
@@ -1,3 +1,8 @@
+using Ice_Web_API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddDbContext<WebDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("BookStore")));
 
 var app = builder.Build();
 
