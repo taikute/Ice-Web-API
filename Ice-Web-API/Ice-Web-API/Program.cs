75419b011b41804b214b523907fc2daea8562215
Cyan <127851673+taikute@users.noreply.github.com>
2023-04-01 20:58:11 +0700

    Revert "Test API"

--- Ice-Web-API/Ice-Web-API/Program.cs
+++ Ice-Web-API/Ice-Web-API/Program.cs
@@ -1,8 +1,3 @@
-using Ice_Web_API.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,11 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
-builder.Services.AddDbContext<WebDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("BookStore")));
 
 var app = builder.Build();
 
