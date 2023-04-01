b8b2bef8ecc9b40a6d09cfb154f8ba87c046c385
Cyan <anhtai05102002@gmail.com>
2023-04-01 20:35:45 +0700

    CRUD
    Add data, model, automaper

---
+++ Ice-Web-API/Ice-Web-API/Helpers/WebMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ice_Web_API.Data;
+using Ice_Web_API.Models;
+
+namespace Ice_Web_API.Helpers
+{
+    public class WebMapper : Profile
+    {
+        public WebMapper()
+        {
+            CreateMap<Book, BookModel>().ReverseMap();
+        }
+    }
+}
